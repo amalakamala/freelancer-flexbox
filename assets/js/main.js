2e6dc07c65ffb8704a2c7d9bbd77fb1ad26e2c47
amalakamala <amalakamala.a@gmail.com>
2017-06-14 23:27:47 -0400

    htm, js y css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,53 +1,73 @@
-/*
-
 function resizeHeaderOnScroll() {
   var distanciaY = window.pageYOffset || document.documentElement.scrollTop;
 
+  /*
   window.pageYOffset:  devuelve los píxeles del documento actual.
   scrollTop : devuelve el número de píxeles contenido de un elemento se desplaza verticalmente.
+  */
 
   var limite = 100;
   var elNav = document.getElementById('navegador'); 
-  var elHide = document.getElementById('a-hide');
-  var lasA = Array.from(document.getElementsByClassName('estilo-a'));
-  var navEspecial = document.getElementById('log-in');
-  var logoW = document.getElementById('logo-white');
-  var logoP = document.getElementById('logo-pink');
-
 
   if (distanciaY > limite) {
-    elNav.classList.add("menu-dos"); 
-
-    elHide.classList.remove("nav-hide");
-    elHide.classList.add("estilo-a-scroll"); 
-
-    lasA.forEach(function(e){
-      e.classList.add("color-a"); 
-    });
-    navEspecial.classList.remove("nav-especial");
-    navEspecial.classList.add("nav-especial-scroll");
-
-    logoP.classList.remove("hide");    
-    logoW.classList.add("hide");
-    logoP.classList.add("logo");
-
+    elNav.classList.add("el-navegador");
+    elNav.classList.remove("el-menu");
   }else{
-    elNav.classList.remove("menu-dos");
-
-    elHide.classList.add("nav-hide")
-
-    lasA.forEach(function(e){
-      e.classList.remove("color-a"); 
-    }); 
-    navEspecial.classList.add("nav-especial");
-
-    logoP.classList.add("hide");
-    logoW.classList.remove("hide");  
-    logoP.classList.remove("logo");
-
+    elNav.classList.remove("el-navegador");
+    elNav.classList.add("el-menu");    
   }
 }
 
 window.addEventListener('scroll', resizeHeaderOnScroll);
 
-*/
+
+
+
+var boton = document.getElementById("btn-send");
+boton.addEventListener("click", function(e){
+  e.preventDefault();
+  
+  var nombre = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var phone = document.getElementById("phone").value;
+  var message = document.getElementById("message").value;
+
+  var nombreValido = document.getElementsByClassName("form-ingreso")[0];
+  var emailValido = document.getElementsByClassName("form-ingreso")[1];
+  var phoneValido = document.getElementsByClassName("form-ingreso")[2];
+  var mensajeValido = document.getElementsByClassName("form-ingreso")[3];
+
+
+    if(!(/^([A-ZÁÉÍÓÚ]{1}[a-zñáéíóú]{2,11})+$/.test(name))){
+    var respuestaNom = document.createElement("span");  
+    respuestaNom.classList.add("el-span")
+      var textoNom = document.createTextNode("•  La primera letra debe ser en mayúscula");
+      respuestaNom.appendChild(textoNom);   
+      nombreValido.appendChild(respuestaNom);
+    }  
+
+    if(!(/^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$/.test(email))) {
+      var respuestaEmail = document.createElement("span");  
+      respuestaEmail.classList.add("el-span")
+      var textoEmail = document.createTextNode("•  Ingrese su correo correctamente");
+      respuestaEmail.appendChild(textoEmail);   
+      emailValido.appendChild(respuestaEmail);
+    }
+
+    if(!(/\+569[0-9]{8}/.test(phone))){
+      var respuestaPhone = document.createElement("span");  
+      respuestaPhone.classList.add("el-span")
+      var textoPhone = document.createTextNode("•  Ingrese su teléfono correctamente");
+      respuestaPhone.appendChild(textoPhone);   
+      phoneValido.appendChild(respuestaPhone);      
+    }
+
+    if(message == ""){
+      var respuestaMensaje = document.createElement("span");  
+     respuestaMensaje.classList.add("el-span")
+      var textoMensaje = document.createTextNode("•  Ingrese su mensaje");
+      respuestaMensaje.appendChild(textoMensaje);   
+      mensajeValido.appendChild(respuestaMensaje);
+    }
+
+ })
