7f5a604acb8f5446c3e40a0337a7d2ebe8bd2e44
amalakamala <amalakamala.a@gmail.com>
2017-06-15 18:06:23 -0400

    fin.. por ahora

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,36 +38,43 @@ boton.addEventListener("click", function(e){
   var mensajeValido = document.getElementsByClassName("form-ingreso")[3];
 
 
-    if(!(/^([A-ZÁÉÍÓÚ]{1}[a-zñáéíóú]{2,11})+$/.test(name))){
-    var respuestaNom = document.createElement("span");  
-    respuestaNom.classList.add("el-span")
+
+      if(!(/^([A-ZÁÉÍÓÚ]{1}[a-zñáéíóú]{2,11})+$/.test(name))){
+      var respuestaNom = document.createElement("span");  
+      respuestaNom.classList.add("el-span")
       var textoNom = document.createTextNode("•  La primera letra debe ser en mayúscula");
       respuestaNom.appendChild(textoNom);   
       nombreValido.appendChild(respuestaNom);
-    }  
+     }  
 
-    if(!(/^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$/.test(email))) {
-      var respuestaEmail = document.createElement("span");  
-      respuestaEmail.classList.add("el-span")
-      var textoEmail = document.createTextNode("•  Ingrese su correo correctamente");
-      respuestaEmail.appendChild(textoEmail);   
-      emailValido.appendChild(respuestaEmail);
-    }
 
-    if(!(/\+569[0-9]{8}/.test(phone))){
-      var respuestaPhone = document.createElement("span");  
-      respuestaPhone.classList.add("el-span")
-      var textoPhone = document.createTextNode("•  Ingrese su teléfono correctamente");
-      respuestaPhone.appendChild(textoPhone);   
-      phoneValido.appendChild(respuestaPhone);      
-    }
+      if(!(/^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$/.test(email))) {
+        var respuestaEmail = document.createElement("span");  
+        respuestaEmail.classList.add("el-span")
+        var textoEmail = document.createTextNode("•  Ingrese su correo correctamente");
+        respuestaEmail.appendChild(textoEmail);   
+        emailValido.appendChild(respuestaEmail);
+      }
 
-    if(message == ""){
-      var respuestaMensaje = document.createElement("span");  
-     respuestaMensaje.classList.add("el-span")
-      var textoMensaje = document.createTextNode("•  Ingrese su mensaje");
-      respuestaMensaje.appendChild(textoMensaje);   
-      mensajeValido.appendChild(respuestaMensaje);
-    }
+
+
+      if(!(/\+569[0-9]{8}/.test(phone))){
+        var respuestaPhone = document.createElement("span");  
+        respuestaPhone.classList.add("el-span")
+        var textoPhone = document.createTextNode("•  Ingrese su teléfono correctamente");
+        respuestaPhone.appendChild(textoPhone);   
+        phoneValido.appendChild(respuestaPhone);      
+      }
+     
+
+
+      if(message == ""){
+        var respuestaMensaje = document.createElement("span");  
+       respuestaMensaje.classList.add("el-span")
+        var textoMensaje = document.createTextNode("•  Ingrese su mensaje");
+        respuestaMensaje.appendChild(textoMensaje);   
+        mensajeValido.appendChild(respuestaMensaje);
+      }
+
 
  })
